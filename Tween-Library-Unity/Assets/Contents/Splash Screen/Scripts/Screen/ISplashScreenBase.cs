33eeed4bf3bf33d9f6b5e07a2acaa0cdebe23d3f
Rukesh22 <trukeshreddy2274@gmail.com>
2025-01-30 12:09:03 +0530

    DOTween Package Project
    This Project used to create an animated icon dashboard and custom splash screen.

---
+++ Tween-Library-Unity/Assets/Contents/Splash Screen/Scripts/Screen/ISplashScreenBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.SplashScreen
+{
+	public abstract class SplashScreenBase : MonoBehaviour
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="Manager"></param>
+		public abstract void Initialize(SplashManager Manager);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public abstract void ResetUI();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="callBack"></param>
+		public abstract void Play (System.Action callBack);
+	}
+}
---
+++ Tween-Library-Unity/README.md
@@ -0,0 +1,7 @@
+# Tween-Library-Unity3D
+DOTween Things
+- Splash Screen
+- Tween Selectable
+
+Plugin Used:
+- DOTween: https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676
